5bc1009915dafbabc6db9c23c0f0f7b904ad39ec
Sudharsan <sudharsandharma007@gmail.com>
2025-12-23 20:06:33 +0530

    Added menu page and payment page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Menu from "./pages/menu";
 import Cart from "./pages/cart";
+import Payment from "./pages/payment";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/menu" element={<Menu />} />
         <Route path="/cart" element={<Cart />} />
+        <Route path="/payment" element={<Payment />} />
       </Routes>
     </BrowserRouter>
   );
