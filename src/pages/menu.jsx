a195284734d4ac018601234f61a95aa075b40590
Bhawanitha N <nbhawanitha@gmail.com>
2025-12-30 12:40:24 +0530

    fix Menu.jsx filename case issue

--- src/pages/menu.jsx
+++ src/pages/menu.jsx
@@ -1,4 +1,16 @@
-function menu() {
-  return <h1>Menu Page</h1>;
+import FoodItem from "../components/FoodItem";
+
+function Menu() {
+  return (
+    <div>
+      <h1>Menu</h1>
+      <hr />
+
+      <FoodItem name="Pizza" price={199} />
+      <FoodItem name="Burger" price={99} />
+      <FoodItem name="Biryani" price={249} />
+    </div>
+  );
 }
-export default menu;
+
+export default Menu;
